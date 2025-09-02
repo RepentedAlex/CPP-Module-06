22f90245330ff037d6749be30e7069be8d2eb3bd
apetitco <apetitco@student.42.fr>
2025-09-02 14:46:23 +0200

    removed obsolete things in ex00

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -2,29 +2,6 @@
 
 #include <iostream>
 
-#define BLUE	"\033[1;95m"
-#define CYAN	"\033[36m"
-#define YELLOW	"\033[4;33m"
-#define RESET	"\033[0m"
-
-#define PRINT_SECTION(x) \
-do { \
-	std::cout << std::endl << BLUE "=== " x " ===" RESET << std::endl << std::endl; \
-} while (0)
-
-
-#define PRINT_SUBSECTION(x) \
-do { \
-	std::cout << CYAN "~ " x " ~" RESET << std::endl; \
-} while (0)
-
-#define PRINT_TEST(x) \
-do { \
-	std::cout << YELLOW x RESET << std::endl; \
-} while (0)
-
-#define PRINT(x) do { std::cout << x << std::endl; } while (0)
-
 int	main(int argc, char* argv[]) {
 	if (argc != 2) {
 		std::cout << "Usage: " << argv[0] << " <argument>" << std::endl;
